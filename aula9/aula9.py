eff0f317527b2fdfaefda80750ab8d03ffa531a1
wendellcs <wendellcs131@gmail.com>
2026-04-09 19:27:55 -0300

    Estudo sobre módulos

---
+++ aula9/aula9.py
@@ -0,0 +1,19 @@
+# Módulos
+
+# Importando o módulo math
+import math
+# O módulo 'os' permite interagir com o sistema operacional
+import os 
+import modulo
+
+# Função usada para calcular potência.
+print(math.pow(5, 5))
+
+# Arredonda para cima
+print(math.ceil(4.1))
+# Arredonda para baixo
+print(math.floor(4.8))
+# Arrendonda para o inteiro mais próximo.
+print(math.trunc(3.2))
+
+print(modulo.soma(5, 10))
